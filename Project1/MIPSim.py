0f65885beec5b51e9d0b9c12d9070c61b8f311e7
brightzh <brightzh@outlook.com>
2019-10-25 15:37:02 +0800

    完成反汇编的指令部分

--- Project1/MIPSim.py
+++ Project1/MIPSim.py
@@ -5,159 +5,144 @@
 # @Software: PyCharm
 
 START_ADDRESS = 256  # 起始地址
-instruction_sequence = {}  # 指令序列
+INSTRUCTION_SEQUENCE = {}  # 指令序列
+INSTRUCTION_COUNT = 0  # 指令条数
 memory_space = {}  # 模拟存储器（存放data）
 
 
-def left_shift(input_str, shamt):  # 二进制左移
-    output_str = input_str[shamt:]
-    for i in range(shamt):
-        output_str = output_str + '0'
-    return output_str
-
-
-def disassembler(input_file_name, start_address):  # 反汇编器，将机器码还原为指令序列，并写入存储器内容
+def disassembler_instruction(input_file_name, output_file_name, start_address):  # 反汇编器（第一部分），将机器码还原为指令序列， 并写入文件disassembly.txt
+    instruction_count = 0
+    instruction_sequence = {}
     current_address = start_address
     input_file = open(input_file_name)
+    output_file = open(output_file_name, 'w')
     input_line = input_file.readline()
     while input_line:  # 指令段到BREAK结束
-        print(input_line[0:32], end='\t')
+        # print(input_line[0:32], end='\t')
         if input_line[0:2] == '01':  # Category-1
             if input_line[2:6] == '0000':  # J target
-                print(current_address, end='\t')
                 instruction = 'J #' + str(int(input_line[6:32]+'00', 2))
-                print(instruction, end='\t')
             elif input_line[2:6] == '0001':  # JR rs
-                print(current_address, end='\t')
                 instruction = 'JR ' + 'R' + str(int(input_line[6:11], 2))
-                print(instruction, end='\t')
             elif input_line[2:6] == '0010':  # BEQ rs, rt, offset
-                print(current_address, end='\t')
-                instruction = 'BEQ '
                 sign_extend_offset = input_line[16:32] + '00'  # 18bits
                 if sign_extend_offset[0] == '0':  # 符号位为0，offset为正
-                    decimal_sign_extend_offset = int(sign_extend_offset, 2)
+                    target_offset = int(sign_extend_offset, 2)
+                elif sign_extend_offset[0] == '1':  # 符号位为1，offset为负
+                    pass  # TODO
+                instruction = 'BEQ ' + 'R' + str(int(input_line[6:11], 2)) + ", " \
+                              + 'R' + str(int(input_line[11:16], 2)) + ", " \
+                              + '#' + str(target_offset)
+            elif input_line[2:6] == '0011':  # BLTZ rs, offset
+                sign_extend_offset = input_line[16:32] + '00'  # 18bits
+                if sign_extend_offset[0] == '0':  # 符号位为0，offset为正
+                    target_offset = int(sign_extend_offset, 2)
                 elif sign_extend_offset[0] == '1':  # 符号位为1，offset为负
                     pass
-                instruction = instruction + 'R' + str(int(input_line[6:11], 2)) + ", " \
-                              + 'R' + str(int(input_line[11:16], 2)) + ", " \
-                              + '#' + str(decimal_sign_extend_offset)
-                print(instruction, end='\t')
-            elif input_line[2:6] == '0011':  # BLTZ
-                print(current_address, end='\t')
-                instruction = 'BLTZ '
-                print(instruction, end='\t')
-            elif input_line[2:6] == '0100':  # BGTZ
-                print(current_address, end='\t')
-                instruction = 'BGTZ '
-                print(instruction, end='\t')
+                instruction = 'BLTZ ' + 'R' + str(int(input_line[6:11], 2)) + ", " \
+                              + '#' + str(target_offset)
+            elif input_line[2:6] == '0100':  # BGTZ rs, offset
+                sign_extend_offset = input_line[16:32] + '00'  # 18bits
+                if sign_extend_offset[0] == '0':  # 符号位为0，offset为正
+                    target_offset = int(sign_extend_offset, 2)
+                elif sign_extend_offset[0] == '1':  # 符号位为1，offset为负
+                    pass
+                instruction = 'BGTZ ' + 'R' + str(int(input_line[6:11], 2)) + ", " \
+                              + '#' + str(target_offset)
             elif input_line[2:6] == '0101':  # BREAK
-                print(current_address, end='\t')
                 instruction = 'BREAK'
-                print(instruction, end='\t')
-            elif input_line[2:6] == '0110':  # SW
-                print(current_address, end='\t')
-                instruction = 'SW '
-                print(instruction, end='\t')
-            elif input_line[2:6] == '0111':  # LW
-                print(current_address, end='\t')
-                instruction = 'LW '
-                print(instruction, end='\t')
-            elif input_line[2:6] == '1000':  # SLL
-                print(current_address, end='\t')
-                instruction = 'SLL '
-                print(instruction, end='\t')
-            elif input_line[2:6] == '1001':  # SRL
-                print(current_address, end='\t')
-                instruction = 'SRL '
-                print(instruction, end='\t')
-            elif input_line[2:6] == '1010':  # SRA
-                print(current_address, end='\t')
-                instruction = 'SRA '
-                print(instruction, end='\t')
+            elif input_line[2:6] == '0110':  # SW rt, offset(base)
+                if input_line[16] == '0': # 符号位为0，offset为正
+                    decimal_offset = int(input_line[16:32], 2)
+                elif input_line[16] == '1': # 符号位为1，offset为负
+                    pass
+                instruction = 'SW ' + 'R' + str(int(input_line[11:16], 2)) + ", " \
+                              + str(decimal_offset) + "(R" + str(int(input_line[6:11], 2)) + ')'
+            elif input_line[2:6] == '0111':  # LW rt, offset(base)
+                if input_line[16] == '0': # 符号位为0，offset为正
+                    decimal_offset = int(input_line[16:32], 2)
+                elif input_line[16] == '1': # 符号位为1，offset为负
+                    pass
+                instruction = 'LW ' + 'R' + str(int(input_line[11:16], 2)) + ", " \
+                              + str(decimal_offset) + "(R" + str(int(input_line[6:11], 2)) + ')'
+            elif input_line[2:6] == '1000':  # SLL rd, rt, sa
+                instruction = 'SLL ' + 'R' + str(int(input_line[16:21], 2)) + ", " \
+                              + 'R' + str(int(input_line[11:16], 2)) + ", " \
+                              + '#' + str(int(input_line[21:26], 2))
+            elif input_line[2:6] == '1001':  # SRL rd, rt, sa
+                instruction = 'SRL ' + 'R' + str(int(input_line[16:21], 2)) + ", " \
+                              + 'R' + str(int(input_line[11:16], 2)) + ", " \
+                              + '#' + str(int(input_line[21:26], 2))
+            elif input_line[2:6] == '1010':  # SRA rd, rt, sa (Shift Word Right Arithmetic)
+                instruction = 'SRA ' + 'R' + str(int(input_line[16:21], 2)) + ", " \
+                              + 'R' + str(int(input_line[11:16], 2)) + ", " \
+                              + '#' + str(int(input_line[21:26], 2))
             elif input_line[2:6] == '1011':   # NOP（No Operation）
-                print(current_address, end='\t')
                 instruction = 'NOP'
-                print(instruction, end='\t')
-            print("Category-1")
         elif input_line[0:2] == '11':  # Category-2
             if input_line[2:6] == '0000':  # ADD
-                print(current_address, end='\t')
                 instruction = 'ADD ' + 'R' + str(int(input_line[16:21], 2)) + ", " \
                               + 'R' + str(int(input_line[6:11], 2)) + ", " \
                               + 'R' + str(int(input_line[11:16], 2))
-                print(instruction, end='\t')
             elif input_line[2:6] == '0001':  # SUB
-                print(current_address, end='\t')
                 instruction = 'SUB ' + 'R' + str(int(input_line[16:21], 2)) + ", " \
                               + 'R' + str(int(input_line[6:11], 2)) + ", " \
                               + 'R' + str(int(input_line[11:16], 2))
-                print(instruction, end='\t')
             elif input_line[2:6] == '0010':  # MUL
-                print(current_address, end='\t')
                 instruction = 'MUL ' + 'R' + str(int(input_line[16:21], 2)) + ", " \
                               + 'R' + str(int(input_line[6:11], 2)) + ", " \
                               + 'R' + str(int(input_line[11:16], 2))
-                print(instruction, end='\t')
             elif input_line[2:6] == '0011':  # AND
-                print(current_address, end='\t')
                 instruction = 'AND ' + 'R' + str(int(input_line[16:21], 2)) + ", " \
                               + 'R' + str(int(input_line[6:11], 2)) + ", " \
                               + 'R' + str(int(input_line[11:16], 2))
-                print(instruction, end='\t')
             elif input_line[2:6] == '0100':  # OR
-                print(current_address, end='\t')
                 instruction = 'OR ' + 'R' + str(int(input_line[16:21], 2)) + ", " \
                               + 'R' + str(int(input_line[6:11], 2)) + ", " \
                               + 'R' + str(int(input_line[11:16], 2))
-                print(instruction, end='\t')
             elif input_line[2:6] == '0101':  # XOR
-                print(current_address, end='\t')
                 instruction = 'XOR ' + 'R' + str(int(input_line[16:21], 2)) + ", " \
                               + 'R' + str(int(input_line[6:11], 2)) + ", " \
                               + 'R' + str(int(input_line[11:16], 2))
-                print(instruction, end='\t')
             elif input_line[2:6] == '0110':  # NOR
-                print(current_address, end='\t')
                 instruction = 'NOR ' + 'R' + str(int(input_line[16:21], 2)) + ", " \
                               + 'R' + str(int(input_line[6:11], 2)) + ", " \
                               + 'R' + str(int(input_line[11:16], 2))
-                print(instruction, end='\t')
             elif input_line[2:6] == '0111':  # SLT
-                print(current_address, end='\t')
                 instruction = 'SLT ' + 'R' + str(int(input_line[16:21], 2)) + ", " \
                               + 'R' + str(int(input_line[6:11], 2)) + ", " \
                               + 'R' + str(int(input_line[11:16], 2))
-                print(instruction, end='\t')
             elif input_line[2:6] == '1000':  # ADDI
-                print(current_address, end='\t')
                 instruction = 'ADDI ' + 'R' + str(int(input_line[11:16], 2)) + ', ' \
                               + 'R' + str(int(input_line[6:11], 2)) + ", " \
                               + '#' + str(int(input_line[16:32], 2))
-                print(instruction, end='\t')
             elif input_line[2:6] == '1001':  # ANDI
-                print(current_address, end='\t')
                 instruction = 'ANDI ' + 'R' + str(int(input_line[11:16], 2)) + ', ' \
                               + 'R' + str(int(input_line[6:11], 2)) + ", " \
                               + '#' + str(int(input_line[16:32], 2))
-                print(instruction, end='\t')
             elif input_line[2:6] == '1010':  # ORI
-                print(current_address, end='\t')
                 instruction = 'ORI ' + 'R' + str(int(input_line[11:16], 2)) + ', ' \
                               + 'R' + str(int(input_line[6:11], 2)) + ", " \
                               + '#' + str(int(input_line[16:32], 2))
-                print(instruction, end='\t')
             elif input_line[2:6] == '1011':  # XORI
-                print(current_address, end='\t')
                 instruction = 'XORI ' + 'R' + str(int(input_line[11:16], 2)) + ', ' \
                               + 'R' + str(int(input_line[6:11], 2)) + ", " \
                               + '#' + str(int(input_line[16:32], 2))
-                print(instruction, end='\t')
-            print("Category-2")
-        if input_line[0:32] == '01010100000000000000000000001101':
-            break
+        print(input_line[0:32] + '\t' + str(current_address) + '\t' + instruction)
+        output_file.write(input_line[0:32] + '\t' + str(current_address) + '\t' + instruction + '\n')
+        instruction_count = instruction_count + 1
+        instruction_sequence[current_address] = instruction
         current_address = current_address + 4
+        if instruction == 'BREAK':
+            break
         input_line = input_file.readline()
+    output_file.close()
+    return instruction_count, instruction_sequence
 
 
-disassembler('sample.txt', START_ADDRESS)
+def disassembler_memory(input_file_name, start_address):  # 反汇编器(第二部分)，将指令序列后的补码序列写入到存储空间（data），并写入文件disassembly.txt
+    pass
+
+
+INSTRUCTION_COUNT, INSTRUCTION_SEQUENCE = disassembler_instruction('sample.txt', 'disassembly.txt',START_ADDRESS)
+
