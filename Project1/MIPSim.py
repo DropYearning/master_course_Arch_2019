4aa6f48d36940864edae8449f82e0b785f948b4b
brightzh <brightzh@outlook.com>
2019-10-26 11:05:16 +0800

    修复offset等偏移量可能为负的问题

--- Project1/MIPSim.py
+++ Project1/MIPSim.py
@@ -11,10 +11,11 @@ INSTRUCTION_SEQUENCE = {}  # 指令序列
 INSTRUCTION_COUNT = 0  # 指令条数
 
 MIPS_STATUS = {
-    'PC': START_ADDRESS,  # 程序计数器
+    'CycleNumber': 0,  # 当前执行指令的周期数
+    'PC': START_ADDRESS - 4,  # 程序计数器(当前指令)
+    'NPC': START_ADDRESS,  # 程序计数器（下一条指令）
     'Registers': [0]*32,  # 32个MIPS寄存器
     'Data': {},  # 模拟的存储器空间
-
 }
 
 
@@ -45,112 +46,143 @@ def disassembler_instruction(input_file_name, output_file_name, start_address): 
         if input_line[0:2] == '01':  # Category-1
             if input_line[2:6] == '0000':  # J target
                 instruction = 'J #' + str(int(input_line[6:32]+'00', 2))
+
             elif input_line[2:6] == '0001':  # JR rs
                 instruction = 'JR ' + 'R' + str(int(input_line[6:11], 2))
+
             elif input_line[2:6] == '0010':  # BEQ rs, rt, offset
-                sign_extend_offset = input_line[16:32] + '00'  # 18bits
+                sign_extend_offset = input_line[16:32] + '00'  # 18bits sign_extend_offset
                 if sign_extend_offset[0] == '0':  # 符号位为0，offset为正
                     target_offset = int(sign_extend_offset, 2)
                 elif sign_extend_offset[0] == '1':  # 符号位为1，offset为负
-                    pass  # TODO
+                    target_offset = twos_complement_to_value(sign_extend_offset)
                 instruction = 'BEQ ' + 'R' + str(int(input_line[6:11], 2)) + ", " \
                               + 'R' + str(int(input_line[11:16], 2)) + ", " \
                               + '#' + str(target_offset)
+
             elif input_line[2:6] == '0011':  # BLTZ rs, offset
-                sign_extend_offset = input_line[16:32] + '00'  # 18bits
+                sign_extend_offset = input_line[16:32] + '00'  # 18bits sign_extend_offset
                 if sign_extend_offset[0] == '0':  # 符号位为0，offset为正
                     target_offset = int(sign_extend_offset, 2)
                 elif sign_extend_offset[0] == '1':  # 符号位为1，offset为负
-                    pass
+                    target_offset = twos_complement_to_value(sign_extend_offset)
                 instruction = 'BLTZ ' + 'R' + str(int(input_line[6:11], 2)) + ", " \
                               + '#' + str(target_offset)
+
             elif input_line[2:6] == '0100':  # BGTZ rs, offset
-                sign_extend_offset = input_line[16:32] + '00'  # 18bits
+                sign_extend_offset = input_line[16:32] + '00'  # 18bits sign_extend_offset
                 if sign_extend_offset[0] == '0':  # 符号位为0，offset为正
                     target_offset = int(sign_extend_offset, 2)
                 elif sign_extend_offset[0] == '1':  # 符号位为1，offset为负
-                    pass
+                    target_offset = twos_complement_to_value(sign_extend_offset)
                 instruction = 'BGTZ ' + 'R' + str(int(input_line[6:11], 2)) + ", " \
                               + '#' + str(target_offset)
+
             elif input_line[2:6] == '0101':  # BREAK
                 instruction = 'BREAK'
+
             elif input_line[2:6] == '0110':  # SW rt, offset(base)
-                if input_line[16] == '0': # 符号位为0，offset为正
+                if input_line[16] == '0':  # 符号位为0，offset为正
                     decimal_offset = int(input_line[16:32], 2)
-                elif input_line[16] == '1': # 符号位为1，offset为负
-                    pass
+                elif input_line[16] == '1':  # 符号位为1，offset为负
+                    decimal_offset = twos_complement_to_value(input_line[16:32])
                 instruction = 'SW ' + 'R' + str(int(input_line[11:16], 2)) + ", " \
                               + str(decimal_offset) + "(R" + str(int(input_line[6:11], 2)) + ')'
+
             elif input_line[2:6] == '0111':  # LW rt, offset(base)
                 if input_line[16] == '0': # 符号位为0，offset为正
                     decimal_offset = int(input_line[16:32], 2)
                 elif input_line[16] == '1': # 符号位为1，offset为负
-                    pass
+                    decimal_offset = twos_complement_to_value(input_line[16:32])
                 instruction = 'LW ' + 'R' + str(int(input_line[11:16], 2)) + ", " \
                               + str(decimal_offset) + "(R" + str(int(input_line[6:11], 2)) + ')'
-            elif input_line[2:6] == '1000':  # SLL rd, rt, sa
+
+            elif input_line[2:6] == '1000':  # SLL rd, rt, sa [Shift Word Left Logical]
                 instruction = 'SLL ' + 'R' + str(int(input_line[16:21], 2)) + ", " \
                               + 'R' + str(int(input_line[11:16], 2)) + ", " \
                               + '#' + str(int(input_line[21:26], 2))
-            elif input_line[2:6] == '1001':  # SRL rd, rt, sa
+
+            elif input_line[2:6] == '1001':  # SRL rd, rt, sa [Shift Word Right Logical]
                 instruction = 'SRL ' + 'R' + str(int(input_line[16:21], 2)) + ", " \
                               + 'R' + str(int(input_line[11:16], 2)) + ", " \
                               + '#' + str(int(input_line[21:26], 2))
-            elif input_line[2:6] == '1010':  # SRA rd, rt, sa (Shift Word Right Arithmetic)
+
+            elif input_line[2:6] == '1010':  # SRA rd, rt, sa [Shift Word Right Arithmetic]
                 instruction = 'SRA ' + 'R' + str(int(input_line[16:21], 2)) + ", " \
                               + 'R' + str(int(input_line[11:16], 2)) + ", " \
                               + '#' + str(int(input_line[21:26], 2))
+
             elif input_line[2:6] == '1011':   # NOP（No Operation）
                 instruction = 'NOP'
+
         elif input_line[0:2] == '11':  # Category-2
-            if input_line[2:6] == '0000':  # ADD
+            if input_line[2:6] == '0000':  # ADD rd, rs, rt
                 instruction = 'ADD ' + 'R' + str(int(input_line[16:21], 2)) + ", " \
                               + 'R' + str(int(input_line[6:11], 2)) + ", " \
                               + 'R' + str(int(input_line[11:16], 2))
+
             elif input_line[2:6] == '0001':  # SUB
                 instruction = 'SUB ' + 'R' + str(int(input_line[16:21], 2)) + ", " \
                               + 'R' + str(int(input_line[6:11], 2)) + ", " \
                               + 'R' + str(int(input_line[11:16], 2))
-            elif input_line[2:6] == '0010':  # MUL
+
+            elif input_line[2:6] == '0010':  # MUL rd, rs, rt [rd ← rs × rt]
                 instruction = 'MUL ' + 'R' + str(int(input_line[16:21], 2)) + ", " \
                               + 'R' + str(int(input_line[6:11], 2)) + ", " \
                               + 'R' + str(int(input_line[11:16], 2))
-            elif input_line[2:6] == '0011':  # AND
+
+            elif input_line[2:6] == '0011':  # AND rd, rs, rt
                 instruction = 'AND ' + 'R' + str(int(input_line[16:21], 2)) + ", " \
                               + 'R' + str(int(input_line[6:11], 2)) + ", " \
                               + 'R' + str(int(input_line[11:16], 2))
-            elif input_line[2:6] == '0100':  # OR
+
+            elif input_line[2:6] == '0100':  # OR rd, rs, rt
                 instruction = 'OR ' + 'R' + str(int(input_line[16:21], 2)) + ", " \
                               + 'R' + str(int(input_line[6:11], 2)) + ", " \
                               + 'R' + str(int(input_line[11:16], 2))
-            elif input_line[2:6] == '0101':  # XOR
+
+            elif input_line[2:6] == '0101':  # XOR rd, rs, rt
                 instruction = 'XOR ' + 'R' + str(int(input_line[16:21], 2)) + ", " \
                               + 'R' + str(int(input_line[6:11], 2)) + ", " \
                               + 'R' + str(int(input_line[11:16], 2))
+
             elif input_line[2:6] == '0110':  # NOR
                 instruction = 'NOR ' + 'R' + str(int(input_line[16:21], 2)) + ", " \
                               + 'R' + str(int(input_line[6:11], 2)) + ", " \
                               + 'R' + str(int(input_line[11:16], 2))
+
             elif input_line[2:6] == '0111':  # SLT
                 instruction = 'SLT ' + 'R' + str(int(input_line[16:21], 2)) + ", " \
                               + 'R' + str(int(input_line[6:11], 2)) + ", " \
                               + 'R' + str(int(input_line[11:16], 2))
-            elif input_line[2:6] == '1000':  # ADDI
+
+            elif input_line[2:6] == '1000':  # ADDI rt, rs, immediate
+                decimal_imm = int(input_line[16:32], 2) if input_line[16] == '0' else twos_complement_to_value(input_line[16:32])
                 instruction = 'ADDI ' + 'R' + str(int(input_line[11:16], 2)) + ', ' \
                               + 'R' + str(int(input_line[6:11], 2)) + ", " \
-                              + '#' + str(int(input_line[16:32], 2))
-            elif input_line[2:6] == '1001':  # ANDI
+                              + '#' + str(decimal_imm)
+
+            elif input_line[2:6] == '1001':  # ANDI rt, rs, immediate
+                decimal_imm = int(input_line[16:32], 2) if input_line[16] == '0' else twos_complement_to_value(
+                    input_line[16:32])
                 instruction = 'ANDI ' + 'R' + str(int(input_line[11:16], 2)) + ', ' \
                               + 'R' + str(int(input_line[6:11], 2)) + ", " \
-                              + '#' + str(int(input_line[16:32], 2))
-            elif input_line[2:6] == '1010':  # ORI
+                              + '#' + str(decimal_imm)
+
+            elif input_line[2:6] == '1010':  # ORI rt, rs, immediate
+                decimal_imm = int(input_line[16:32], 2) if input_line[16] == '0' else twos_complement_to_value(
+                    input_line[16:32])
                 instruction = 'ORI ' + 'R' + str(int(input_line[11:16], 2)) + ', ' \
                               + 'R' + str(int(input_line[6:11], 2)) + ", " \
-                              + '#' + str(int(input_line[16:32], 2))
-            elif input_line[2:6] == '1011':  # XORI
+                              + '#' + str(decimal_imm)
+
+            elif input_line[2:6] == '1011':  # XORI rt, rs, immediate
+                decimal_imm = int(input_line[16:32], 2) if input_line[16] == '0' else twos_complement_to_value(
+                    input_line[16:32])
                 instruction = 'XORI ' + 'R' + str(int(input_line[11:16], 2)) + ', ' \
                               + 'R' + str(int(input_line[6:11], 2)) + ", " \
-                              + '#' + str(int(input_line[16:32], 2))
+                              + '#' + str(decimal_imm)
+
         # print(input_line[0:32] + '\t' + str(current_address) + '\t' + instruction)
         output_file_pointer.write(input_line[0:32] + '\t' + str(current_address) + '\t' + instruction + '\n')
         instruction_count = instruction_count + 1
@@ -182,7 +214,9 @@ def disassembler_memory(input_file_name, output_file_name, start_address):  # �
     return memory_space
 
 
-def print_status(mips_status):  # 输出Registers和Data状态
+def print_status(mips_status):  # 输出某一个Cycle的状态
+    print('--------------------')
+    print("Cycle:" + str(mips_status['CycleNumber']) + '\t' + str(mips_status['PC']) + '\t' + INSTRUCTION_SEQUENCE[mips_status['PC']])
     print("Registers")
     for i in range(32):
         if i % 8 == 0:
@@ -196,7 +230,7 @@ def print_status(mips_status):  # 输出Registers和Data状态
             print(str(mips_status['Registers'][i]), end='\t')
     print("")
     print("Data")
-    word_number = len(mips_status['Data']) #24
+    word_number = len(mips_status['Data'])  # 存储器中的字数
     data_start_address = list(mips_status['Data'])[0]
     for i in range(word_number):
         current_address = data_start_address + i * 4
@@ -209,9 +243,131 @@ def print_status(mips_status):  # 输出Registers和Data状态
     print('')
     print('--------------------')
 
-INSTRUCTION_COUNT, INSTRUCTION_SEQUENCE = disassembler_instruction('sample.txt', 'disassembly.txt', START_ADDRESS)
-MIPS_STATUS['Data'] = disassembler_memory('sample.txt', 'disassembly.txt', START_ADDRESS + INSTRUCTION_COUNT * 4)
+
+def instruction_operation(instruction, old_status):
+    temp_status = old_status
+    temp_status['CycleNumber'] = temp_status['CycleNumber'] + 1
+    temp_status['PC'] = temp_status['NPC']
+    temp_status['NPC'] = temp_status['PC'] + 4  # 非跳转指令 PC = PC + 4
+    op = instruction.split(' ')[0]
+    if op == 'J':  # J target
+        target = instruction[3:]
+        temp_status['NPC'] = target
+
+    elif op == 'JR':  # JR rs [PC ← rs]
+        rs_index = int(instruction[3:])
+        temp_status['NPC'] = temp_status['Registers'][rs_index]
+
+    elif op == 'BEQ':  # BEQ rs, rt, offset 【if rs = rt then branch】
+        rs_index = int(instruction[4:].replace(" ", "").split(',')[0][1:])
+        rt_index = int(instruction[4:].replace(" ", "").split(',')[1][1:])
+        offset = int(instruction[4:].replace(" ", "").split(',')[2][1:])
+        if temp_status['Registers'][rs_index] == temp_status['Registers'][rt_index]:
+            temp_status['NPC'] = temp_status['NPC'] + offset  # TODO BEQ跳转
+
+    elif op == 'BLTZ':  # BLTZ rs, offset [if rs < 0 then branch]
+        rs_index = int(instruction[4:].replace(" ", "").split(',')[0][1:])
+        offset = int(instruction[4:].replace(" ", "").split(',')[1][1:])
+        if temp_status['Registers'][rs_index] < 0:
+            temp_status['NPC'] = temp_status['NPC'] + offset  # TODO BLTZ跳转
+
+    elif op == 'BGTZ':  # BGTZ rs, offset [if rs > 0 then branch]
+        rs_index = int(instruction[4:].replace(" ", "").split(',')[0][1:])
+        offset = int(instruction[4:].replace(" ", "").split(',')[1][1:])
+        if temp_status['Registers'][rs_index] > 0:
+            temp_status['NPC'] = temp_status['NPC'] + offset  # TODO BGTZ跳转
+
+    elif op == 'BREAK':
+        pass  # no operation
+
+    elif op == 'SW':  # SW rt, offset(base) [memory[base+offset] ← rt]
+        rt_index = int(instruction[3:].replace(" ", "").split(',')[0][1:])
+        comma_index = int(instruction[3:].replace(" ", "").index(','))
+        left_parenthesis_index = int(instruction[3:].replace(" ", "").index('('))
+        offset = int(instruction[3:].replace(" ", "")[comma_index + 1:left_parenthesis_index])
+        base = int(instruction[3:].replace(" ", "")[left_parenthesis_index + 2:-1])
+        temp_status['Data'][offset + temp_status['Registers'][base]] = temp_status['Registers'][rt_index]
+
+    elif op == 'LW':  # LW rt, offset(base) [rt ← memory[base+offset]]
+        rt_index = int(instruction[3:].replace(" ", "").split(',')[0][1:])
+        comma_index = int(instruction[3:].replace(" ", "").index(','))
+        left_parenthesis_index = int(instruction[3:].replace(" ", "").index('('))
+        offset = int(instruction[3:].replace(" ", "")[comma_index + 1:left_parenthesis_index])
+        base = int(instruction[3:].replace(" ", "")[left_parenthesis_index + 2:-1])
+        temp_status['Registers'][rt_index] = temp_status['Data'][offset + temp_status['Registers'][base]]
+
+    elif op == 'SLL':  # SLL rd, rt, sa [rd ← rt << sa]
+        rd_index = int(instruction[4:].replace(" ", "").split(',')[0][1:])
+        rt_index = int(instruction[4:].replace(" ", "").split(',')[1][1:])
+        sa = int(instruction[4:].replace(" ", "").split(',')[2][1:])
 
 
+        pass
+    elif op == 'SRL':
+        pass
+    elif op == 'SRA':
+        pass
+    elif op == 'NOP':
+        pass  # no operation
 
-print_status(MIPS_STATUS)
+    elif op == 'ADD':  # ADD rd, rs, rt 【rd ← rs + rt】
+        rd_index = int(instruction[4:].replace(" ", "").split(',')[0][1:])
+        rs_index = int(instruction[4:].replace(" ", "").split(',')[1][1:])
+        rt_index = int(instruction[4:].replace(" ", "").split(',')[2][1:])
+        temp_status['Registers'][rd_index] = temp_status['Registers'][rs_index] + temp_status['Registers'][rt_index]
+
+    elif op == 'SUB':  # SUB rd, rs, rt [rd ← rs - rt]
+        rd_index = int(instruction[4:].replace(" ", "").split(',')[0][1:])
+        rs_index = int(instruction[4:].replace(" ", "").split(',')[1][1:])
+        rt_index = int(instruction[4:].replace(" ", "").split(',')[2][1:])
+        temp_status['Registers'][rd_index] = temp_status['Registers'][rs_index] - temp_status['Registers'][rt_index]
+
+    elif op == 'MUL':  # MUL rd, rs, rt [rd ← rs × rt]
+        rd_index = int(instruction[4:].replace(" ", "").split(',')[0][1:])
+        rs_index = int(instruction[4:].replace(" ", "").split(',')[1][1:])
+        rt_index = int(instruction[4:].replace(" ", "").split(',')[2][1:])
+        temp_status['Registers'][rd_index] = temp_status['Registers'][rs_index] * temp_status['Registers'][rt_index]
+
+    elif op == 'AND':
+        pass
+    elif op == 'OR':
+        pass
+    elif op == 'XOR':
+        pass
+    elif op == 'NOR':
+        pass
+    elif op == 'SLT':
+        pass
+    elif op == 'ADDI':  # ADDI rt, rs, immediate [rt ← rs + immediate]
+        rt_index = int(instruction[4:].replace(" ", "").split(',')[0][1:])
+        rs_index = int(instruction[4:].replace(" ", "").split(',')[1][1:])
+        imm = int(instruction[4:].replace(" ", "").split(',')[2][1:])
+        temp_status['Registers'][rt_index] = temp_status['Registers'][rs_index] + imm
+
+    elif op == 'ANDI':
+        pass
+    elif op == 'ORI':
+        pass
+    elif op == 'XORI':
+        pass
+
+    return temp_status
+
+
+def run():
+    global MIPS_STATUS
+    for inst in INSTRUCTION_SEQUENCE.values():
+        MIPS_STATUS = instruction_operation(inst, MIPS_STATUS)
+        print_status(MIPS_STATUS)
+
+
+if __name__ == '__main__':
+    INSTRUCTION_COUNT, INSTRUCTION_SEQUENCE = disassembler_instruction('sample.txt', 'disassembly.txt', START_ADDRESS)
+    MIPS_STATUS['Data'] = disassembler_memory('sample.txt', 'disassembly.txt', START_ADDRESS + INSTRUCTION_COUNT * 4)
+    print(INSTRUCTION_SEQUENCE)
+    print(MIPS_STATUS['Registers'])
+    print(MIPS_STATUS['Data'])
+    print("\t")
+    # run()
+
+
--- Project1/test.py
+++ Project1/test.py
@@ -4,7 +4,30 @@
 # @File    : test.py
 # @Software: PyCharm
 
-d = {340: -1, 344: -2, 348: -3, 352: 1, 356: 2, 360: 4, 364: -4, 368: 10, 372: 7, 376: 9, 380: 1, 384: 0, 388: -1, 392: 1, 396: -1, 400: 0, 404: 0, 408: 0, 412: 0, 416: 0, 420: 0, 424: 0, 428: 0, 432: 0}
+import re
+MIPS_STATUS={}
+INSTRUCTION_SEQUENCE={256: 'ADD R1, R0, R0', 260: 'ADDI R2, R0, #3', 264: 'BEQ R1, R2, #68', 268: 'SLL R16, R1, #2', 272: 'LW R3, 340(R16)', 276: 'LW R4, 360(R16)', 280: 'LW R5, 380(R16)', 284: 'MUL R5, R3, R4', 288: 'BEQ R5, R0, #28', 292: 'BGTZ R5, #16', 296: 'ADD R6, R3, R4', 300: 'SUB R5, R6, R5', 304: 'J #324', 308: 'BLTZ R5, #4', 312: 'SLL R5, R5, #2', 316: 'J #324', 320: 'ADDI R5, R5, #12', 324: 'SW R5, 380(R16)', 328: 'ADDI R1, R1, #1', 332: 'J #264', 336: 'BREAK'}
+MIPS_STATUS['Registers']=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+MIPS_STATUS['Data']={340: -1, 344: -2, 348: -3, 352: 1, 356: 2, 360: 4, 364: -4, 368: 10, 372: 7, 376: 9, 380: 1, 384: 0, 388: -1, 392: 1, 396: -1, 400: 0, 404: 0, 408: 0, 412: 0, 416: 0, 420: 0, 424: 0, 428: 0, 432: 0}
+
+d = {256: 'ADD R1, R0, R0', 260: 'ADDI R2, R0, #3', 264: 'BEQ R1, R2, #68', 268: 'SLL R16, R1, #2', 272: 'LW R3, 340(R16)', 276: 'LW R4, 360(R16)', 280: 'LW R5, 380(R16)', 284: 'MUL R5, R3, R4', 288: 'BEQ R5, R0, #28', 292: 'BGTZ R5, #16', 296: 'ADD R6, R3, R4', 300: 'SUB R5, R6, R5', 304: 'J #324', 308: 'BLTZ R5, #4', 312: 'SLL R5, R5, #2', 316: 'J #324', 320: 'ADDI R5, R5, #12', 324: 'SW R5, 380(R16)', 328: 'ADDI R1, R1, #1', 332: 'J #264', 336: 'BREAK'}
+
+v = 'SLL R16, R11, #31'
+# rs_index = int(v[4:].replace(" ", "").split(',')[0][1:])
+# rt_index = int(v[4:].replace(" ", "").split(',')[1][1:])
+# offset = int(v[4:].replace(" ", "").split(',')[2][1:])
+# # rt = v[3:].replace(" ", "").split(',')[0][1:]
+# comma_index = v[3:].replace(" ", "").index(',')
+# left_parenthesis_index = v[3:].replace(" ", "").index('(')
+# offset = v[3:].replace(" ", "")[comma_index+1:left_parenthesis_index]
+# base = v[3:].replace(" ", "")[left_parenthesis_index+2:-1]
+
+# print(rs_index, rt_index, offset)
+#
+# for inst in INSTRUCTION_SEQUENCE.values():
+#     print(inst)
+
+a = 8
+print(a << 32)
 
 
-print(list(d)[0])
